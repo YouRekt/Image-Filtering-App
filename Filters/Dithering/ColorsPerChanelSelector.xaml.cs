36e5532976853ca43dbeb89751ae57c2d61d4b38
YouRekt <czarnecki.jurko@gmail.com>
2025-03-28 04:19:15 +0100

    feat: Implemented average dithering and a custom window that allows the user to select the number of colors for dithering

---
+++ Filters/Dithering/ColorsPerChanelSelector.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Image_Filtering_App.Filters.Dithering
+{
+    /// <summary>
+    /// Interaction logic for ColorsPerChanelSelector.xaml
+    /// </summary>
+    public partial class ColorsPerChanelSelector : Window
+    {
+        public int k { get; set; }
+        public ColorsPerChanelSelector(int colors)
+        {
+            InitializeComponent();
+            DataContext = this;
+            k = colors;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ColorsPerChanel.Text, out int colors) && colors >= 2)
+            {
+                k = colors;
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer value (>=2).",
+                                "Invalid Input",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Warning);
+            }
+        }
+    }
+}
